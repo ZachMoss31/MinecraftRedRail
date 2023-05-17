7dfddd7b5ea60fac7d4f87a4dec2cdf8c0b67719
Zach Moss <ganondorf4prez@gmail.com>
2023-05-17 12:10:01 -0600

    Initial creation of Main for the project, includes the initial folder as well

---
+++ Main/Main.py
@@ -0,0 +1,8 @@
+##Initial Python Main Function##
+#This script will serve as the jumping off point for the project in the future
+
+def Main():
+    print("Hello from Main!")
+
+#if __name__ == "Main":
+Main()
